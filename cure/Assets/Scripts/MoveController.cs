07e13ca1b5bb5dc2dcca43a94fa86b1738ce4336
Viktor Rüdén <viktor.ruden2@gmail.com>
2020-10-26 16:51:24 +0100

    started wiith player and projectiles

---
+++ cure/Assets/Scripts/MoveController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    private float movementSpeed = 5f;
+    Rigidbody2D rb2d;
+    Vector2 movement;
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        
+    }
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
