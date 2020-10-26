972cfb21902a8d09700fabbae4d998012ff7a1bc
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-10-26 15:45:57 +0100

    added tags and made virus walk randomly within the camera screen

--- cure/Assets/Scripts/Virus/Virus.cs
+++ cure/Assets/Scripts/Virus/Virus.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Transactions;
+using UnityEditor;
 using UnityEngine;
 
 public class Virus : MonoBehaviour
 {
     int health;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float detectionRadius = 5f;
 
+
+    Vector2 targetPos = new Vector2();
+    Vector2 currentPos = new Vector2();
+
+    
 
     public Virus(VirusType type, int health)
     {
@@ -29,12 +39,26 @@ public class Virus : MonoBehaviour
 
     void Start()
     {
-
+        targetPos = new Vector2(Random.Range(-11, 12), Random.Range(-5,6));
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentPos = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentPos == targetPos)
+        {
+            currentPos = targetPos;
+            targetPos = new Vector2(Random.Range(-11, 12), Random.Range(-5, 6));
+        }
+        else if (currentPos != targetPos)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.fixedDeltaTime);
+        }
 
     }
 
@@ -46,5 +70,11 @@ public class Virus : MonoBehaviour
             // add some fancy death animation 
             Destroy(gameObject);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(currentPos, detectionRadius);
     }
 }
