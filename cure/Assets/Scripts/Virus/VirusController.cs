30dd24a876f9339e5b9ccc3da4c4eff868067a75
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-10-27 09:26:16 +0100

    Update VirusController.cs
    more refactoring

--- cure/Assets/Scripts/Virus/VirusController.cs
+++ cure/Assets/Scripts/Virus/VirusController.cs
@@ -9,105 +9,86 @@ public class VirusController : MonoBehaviour
     [SerializeField] float greenMaxSpawnTime = 5f;
     [SerializeField] Transform greenParent = null;
     float greenSplitTime = 0f;
-    float greenSpawnClock = 0f;
 
     [Header("Orange virus parameters")]
     [SerializeField] float orangeMinSpawnTime = 2f;
     [SerializeField] float orangeMaxSpawnTime = 5f;
     [SerializeField] Transform orangeParent = null;
     float orangeSplitTime = 0f;
-    float orangeSpawnClock = 0f;
 
     [Header("Red virus parameters")]
     [SerializeField] float redMinSpawnTime = 2f;
     [SerializeField] float redMaxSpawnTime = 5f;
     [SerializeField] Transform redParent = null;
     float redSplitTime = 0f;
-    float redSpawnClock = 0f;
 
     [Header("Blue virus parameters")]
     [SerializeField] float blueMinSpawnTime = 2f;
     [SerializeField] float blueMaxSpawnTime = 5f;
     [SerializeField] Transform blueParent = null;
     float blueSplitTime = 0f;
-    float blueSpawnClock = 0f;
 
 
     void Start()
     {
-        SetVirusSplitTimes();
+        SetGreenSplitTime();
+        SetOrangeSplitTime();
+        SetRedSplitTime();
+        SetBlueSplitTime();
     }
 
-    private void SetVirusSplitTimes()
+    private void SetGreenSplitTime()
     {
-        greenSplitTime = Random.Range(greenMinSpawnTime, greenMaxSpawnTime + 1);
-        orangeSplitTime = Random.Range(orangeMinSpawnTime, orangeMaxSpawnTime + 1);
-        redSplitTime = Random.Range(redMinSpawnTime, redMaxSpawnTime + 1);
-        blueSplitTime = Random.Range(blueMinSpawnTime, blueMaxSpawnTime + 1);
+        greenSplitTime = SetRandomTime(greenMinSpawnTime, greenMaxSpawnTime);
+    }
+    private void SetOrangeSplitTime()
+    {
+        orangeSplitTime = SetRandomTime(orangeMinSpawnTime , orangeMaxSpawnTime);
+    }
+    private void SetRedSplitTime()
+    {
+        redSplitTime = SetRandomTime(redMinSpawnTime, redMaxSpawnTime);
+    }
+    private void SetBlueSplitTime()
+    {
+        blueSplitTime = SetRandomTime(blueMinSpawnTime, blueMaxSpawnTime);
+    }
+
+    private float SetRandomTime(float min, float max)
+    {
+        return Random.Range(min, max + 1) + Time.time;
     }
 
     private void FixedUpdate()
     {
-        SplitViruses();
-    }
-
-    private void SplitViruses()
-    {
-        if (greenSpawnClock + greenSplitTime < Time.time)
+        if (greenSplitTime < Time.time)
         {
-            InitializeSpreadOfVirus("Green");
+            SpreadVirus(greenParent.GetComponentsInChildren<Virus>());
+            SetGreenSplitTime();
         }
-        if (orangeSpawnClock + orangeSplitTime < Time.time)
+        if (orangeSplitTime < Time.time)
         {
-            InitializeSpreadOfVirus("Orange");
+            SpreadVirus(orangeParent.GetComponentsInChildren<Virus>());
+            SetOrangeSplitTime();
         }
-        if (redSpawnClock + redSplitTime < Time.time)
+        if (redSplitTime < Time.time)
         {
-            InitializeSpreadOfVirus("Red");
+            SpreadVirus(redParent.GetComponentsInChildren<Virus>());
+            SetRedSplitTime();
         }
-        if (blueSpawnClock + blueSplitTime < Time.time)
+        if (blueSplitTime < Time.time)
         {
-            InitializeSpreadOfVirus("Blue");
-        }
-    }
-
-    private void InitializeSpreadOfVirus(string typeToSpread)
-    {
-        switch(typeToSpread)
-        {
-            case "Green":
-                greenSpawnClock = Time.time;
-                greenSplitTime = Random.Range(greenMinSpawnTime, greenMaxSpawnTime + 1);
-
-                SpreadVirus(greenParent.GetComponentsInChildren<Virus>());
-                break;
-            case "Orange":
-                orangeSpawnClock = Time.time;
-                orangeSplitTime = Random.Range(orangeMinSpawnTime, orangeMaxSpawnTime + 1);
-
-                SpreadVirus(orangeParent.GetComponentsInChildren<Virus>());
-                break;
-            case "Red":
-                redSpawnClock = Time.time;
-                redSplitTime = Random.Range(redMinSpawnTime, redMaxSpawnTime + 1);
-
-                SpreadVirus(redParent.GetComponentsInChildren<Virus>());
-                break;
-            case "Blue":
-                blueSpawnClock = Time.time;
-                blueSplitTime = Random.Range(blueMinSpawnTime, blueMaxSpawnTime + 1);
-
-                SpreadVirus(blueParent.GetComponentsInChildren<Virus>());
-                break;
+            SpreadVirus(blueParent.GetComponentsInChildren<Virus>());
+            SetBlueSplitTime();
         }
     }
 
-    private void SpreadVirus(Virus[] viruses)
+    private void SpreadVirus(Virus[] virions)
     {
-        for (int i = 0; i < viruses.Length; i++)
+        for (int i = 0; i < virions.Length; i++)
         {
-            if (viruses[i] != null)
-                viruses[i].SplitCell();
+            if (virions[i] != null)
+                virions[i].SplitCell();
         }
     }
 }
