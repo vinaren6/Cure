db84939a9cb72a185530b2296fb320fa146aed6f
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-10-27 11:44:48 +0100

    fixed virus animations

--- cure/Assets/Scripts/Vaccine/Vaccine.cs
+++ cure/Assets/Scripts/Vaccine/Vaccine.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class Vaccine : MonoBehaviour
 {
     VaccineType vaccineType;
+    [SerializeField] int vaccineAmount = 3;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "Player")
+        {
+            // use method for adding vaccine to player.
+            GetComponentInParent<VaccineSpawner>().RemoveVaccine(vaccineType);
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- cure/Assets/Scripts/Vaccine/VaccineSpawner.cs
+++ cure/Assets/Scripts/Vaccine/VaccineSpawner.cs
@@ -1,8 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VaccineSpawner : MonoBehaviour
 {
+    [Tooltip("This will determine the minimum spawn point and thus requiers a negative number")]
+    [SerializeField] Vector2 minSpawnpoint = new Vector2();
+    [SerializeField] Vector2 maxSpawnPoint = new Vector2();
+    [SerializeField] Vaccine[] vaccinePrefabs = null;
+    [SerializeField] int maxVaccine = 10;
 
+    [SerializeField] float spawnInterval = 20f;
+
+    float spawnTime = 0f;
+
+    int greenVaccines = 0;
+    int orangeVaccines = 0;
+    int redVaccines = 0;
+    int blueVaccines = 0;
+
+    private void Start()
+    {
+        SpawnVaccines();
+    }
+
+    private void Update()
+    {
+        if(spawnTime < Time.time)
+        {
+            SpawnVaccines();
+        }
+    }
+
+    private void SpawnVaccines()
+    {
+        spawnTime = spawnInterval + Time.time;
+    }
+
+    public void RemoveVaccine(VaccineType vaccineType)
+    {
+
+    }
 }
