bd3e9c96f3fc6680e057d58a419bee27dd3664f3
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-10-29 12:53:33 +0100

    Fixed player boundary
    Made the colliders for player boundary scale with the size of the background.

---
+++ cure/Assets/Scripts/WallColliderScaler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class WallColliderScaler : MonoBehaviour
+{
+    [SerializeField] float width = 2f;
+
+    private void Start()
+    {
+
+        SetWalls();
+    }
+
+    private void SetWalls()
+    {
+        SpriteRenderer spriteRenderer = GetComponentInParent<SpriteRenderer>();
+        BoxCollider2D[] colliders = GetComponents<BoxCollider2D>();
+
+        float xSize = spriteRenderer.size.x;
+        float ySize = spriteRenderer.size.y;
+
+        float xOffset = xSize / 2 + (width / 2);
+        float yOffset = ySize / 2 + (width / 2);
+
+        colliders[0].size = new Vector2(xSize, width);
+        colliders[0].offset = new Vector2(0, yOffset);
+
+        colliders[1].size = new Vector2(width, ySize);
+        colliders[1].offset = new Vector2(xOffset, 0);
+
+        colliders[2].size = new Vector2(xSize, width);
+        colliders[2].offset = new Vector2(0, -yOffset);
+
+        colliders[3].size = new Vector2(width, ySize);
+        colliders[3].offset = new Vector2(-xOffset, 0);
+    }
+}
